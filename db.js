15285f9788d5eb03ceecc72e1441d157ebeb9470
mohamed <mohamedmalek2k23@gmail.com>
2026-04-12 20:07:44 +0100

    db.js done

---
+++ db.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log("❌ DB connection error:", err);
+  } else {
+    console.log("✅ MySQL connected successfully");
+  }
+});
+
+module.exports = db;
